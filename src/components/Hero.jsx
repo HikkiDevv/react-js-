7c350fae7b8fe04de08ba0e18b1e0a249185b0c4
HikkiDev <hikkidevv@gmail.com>
2022-12-24 15:49:54 -0500

    primera pre entrega react js

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './styles/Hero.css';
+
+const Hero = ({ title }) => {
+
+  return (
+    <section className="hero hero-slide-1">
+      <div className="hero-container">
+        <article className="hero-container__text-box">
+          <h1>{title}</h1>
+          <p>
+            ¡La mente muy clara y la cerveza muy fría! Vení a conocer las
+            mejores cervezas del inframundo.
+          </p>
+          <button className="button-primary">Comprar</button>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
